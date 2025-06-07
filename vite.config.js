5b0b58761b37ece702a2dd6893abc2f65c00c76b
Alpha2k24 <141873674+Alpha2k24@users.noreply.github.com>
2025-06-07 21:05:32 +0100

    corrige a base do Vite para '/SolaNzila/'

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/solanzila/',
+  base: '/SolaNzila/',
   plugins: [
     tailwindcss(),
     react()],
